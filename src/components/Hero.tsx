baf13d359931b99c4b4c4350904a7e0269f4cb49
Pedro Gudiño <rscgudino@gmail.com>
2026-09-16 11:15:55 -0300

    Fix typo in Hero component text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -566,7 +566,7 @@ export const Hero: React.FC<HeroProps> = ({
               drop-shadow-[0_4px_18px_rgba(0,0,0,1)]
             "
           >
-            Tu tecnología inteligente.
+            Tecnología inteligente.
           </motion.h1>
 
 
